f0a1064d935bde3f4c8a821d55f379f7c784666a
Nishant Sharma <nishant@nishant-sharma.nishant>
2025-07-01 12:50:10 +0530

    Updated Database.py file and added cred.

--- Database.py
+++ Database.py
@@ -5,7 +5,7 @@ TABLES: Bookrecord, Member, Issue"""
 import mysql.connector
 
 def DatabaseCreate():
-    cnx = mysql.connector.connect(user='root', password='123', host='localhost')
+    cnx = mysql.connector.connect(user='PY_USER_1', password='P@55word', host='localhost')
     Cursor = cnx.cursor()
     Cursor.execute("CREATE DATABASE IF NOT EXISTS Library")
     Cursor.execute("")
@@ -14,7 +14,7 @@ def DatabaseCreate():
 
 
 def TablesCreate():
-    cnx = mysql.connector.connect(user='root', password='123', host='localhost', database='Library')
+    cnx = mysql.connector.connect(user='PY_USER_1', password='P@55word', host='localhost', database='Library')
     Cursor = cnx.cursor()
     Cursor.execute("CREATE TABLE IF NOT EXISTS Bookrecord(Bno int(2), Bname varchar(20), Auth varchar(20), Price int(3), Publ varchar(20), Qty int(2), Date_of_Purchase Date)")
     Cursor.execute("CREATE TABLE IF NOT EXISTS Member(Mno int(2), Mname varchar(20), Date_of_Membership Date, Addr varchar(24), Mob varchar(10))")
