482b14329cbbcd3505089eac1eb58f7abb7a7c05
Nishant Sharma <nishant@nishant-sharma.nishant>
2025-07-01 14:13:04 +0530

    Updated MYSQL Cred in all files

--- Database.py
+++ Database.py
@@ -16,7 +16,7 @@ def DatabaseCreate():
 def TablesCreate():
     cnx = mysql.connector.connect(user='PY_USER_1', password='P@55word', host='localhost', database='Library')
     Cursor = cnx.cursor()
-    Cursor.execute("CREATE TABLE IF NOT EXISTS Bookrecord(Bno int(2), Bname varchar(20), Auth varchar(20), Price int(3), Publ varchar(20), Qty int(2), Date_of_Purchase Date)")
+    Cursor.execute("CREATE TABLE IF NOT EXISTS BookRecord(Bno int(2), Bname varchar(20), Auth varchar(20), Price int(3), Publ varchar(20), Qty int(2), Date_of_Purchase Date)")
     Cursor.execute("CREATE TABLE IF NOT EXISTS Member(Mno int(2), Mname varchar(20), Date_of_Membership Date, Addr varchar(24), Mob varchar(10))")
     Cursor.execute("CREATE TABLE IF NOT EXISTS Issue(Bno int(2), Mno int(2), d_o_issue Date, d_o_ret Date)")
     Cursor.close()
